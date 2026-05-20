83f260e0b860e312f6a39fc9a0a500bed884ef93
Erick Yamato <erick.yamato@me.com>
2026-05-20 10:50:42 -0300

    [ADD] App Icon

---
+++ Image Playgrounds Example/Image Playgrounds Example/Image_Playgrounds_ExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  Image_Playgrounds_ExampleApp.swift
+//  Image Playgrounds Example
+//
+//  Created by Erick Yamato on 20/05/26.
+//
+
+import SwiftUI
+
+@main
+struct Image_Playgrounds_ExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
